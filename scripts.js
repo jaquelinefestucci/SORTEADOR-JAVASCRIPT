ebeae609214559af8470f23a493bdefa07bf2be8
jaquelinefestucci <festuccijaqueline@gmail.com>
2023-12-21 17:09:18 -0300

    Funcional para mega sena

--- scripts.js
+++ scripts.js
@@ -1,50 +1,43 @@
-const trocaimg = document.querySelector(".gif-sorteio");
-const trocaText = document.querySelector(".digite-aqui");
+const trocaText = document.querySelector(".fraseSorteio");
 const result = document.querySelector(".result");
-const button = document.querySelector(".start");
+const novo = document.querySelector(".start");
+const textFinal = document.querySelector(".jogoFinalizado");
+const numberMin = 1;
+const numberMax = 60;
 
-
-
+const jogo = [];
 
 function sorteio() {
-    const min = Math.floor(document.querySelector(".input-de").value);
-    const max = Math.ceil(document.querySelector(".input-para").value);
-    const final = Math.floor(Math.random() * (max - min + 1)) + min;
+  while (jogo.length < 6) {
+    const max = Math.ceil(numberMin);
+    const min = Math.floor(numberMax);
+    const randomNumber = Math.floor(Math.random() * (max - min) + min);
 
-   
-    const inputDe = document.querySelector("#i1").value;
-    const inputPara = document.querySelector("#i2").value;
-
-    if (inputDe === "" || inputPara === "" || min >= max ) {
-        alert ("Favor preencher a numeração corretamente!")
-        return;
+    if (!jogo.includes(randomNumber)) {
+      jogo.push(randomNumber);
     }
-    
-   
-   
-    trocaimg.src = "https://blog.udemy.com/wp-content/uploads/2013/12/shutterstock_36123484-300x300.jpg"
-    button.style.visibility = "visible"
-    trocaText.innerHTML = "Resultado"
-    result.innerHTML = final
-    result.style.fontFamily = "Oswald, sans-serif;"
-    result.style.fontSize = "70px"
-    result.style.cursor = "auto"
-    result.style.color = "white"
-    result.disabled = true;
-     
- };
+  }
+ 
+let sorted = jogo.sort((a, b) => a - b);
 
-function start() {
-    trocaimg.src = "https://gifs.eco.br/wp-content/uploads/2022/11/gifs-de-sorteio-0.gif"
-    trocaText.innerHTML = "Digite aqui os números"
-    result.innerHTML = "Clique <br> Aqui"
-    result.stylefontFamily = "'Oswald', sans-serif;"
-    result.style.fontSize = "20px"
-    button.style.visibility = "hidden"
-    result.style.cursor = "pointer"
-    result.disabled = false;
-    document.querySelector("#i1").value = "";
-    document.querySelector("#i2").value = "";
+trocaText.innerHTML = sorted;
 
+textFinal.innerHTML = "🍀ANOTE SEU JOGO <br> 🤞BOA SORTE!";
+novo.style.visibility = "visible"
+result.style.visibility = "hidden"
 
 }
+
+function start() {
+textFinal.innerHTML = " ";
+jogo.length = 0;
+trocaText.innerHTML = "Sorteio entre 01 - 60 ";
+result.style.cursor = "pointer";
+result.disabled = false;
+novo.style.visibility = "hidden"
+result.style.visibility = "visible"
+
+}
+
+
+
